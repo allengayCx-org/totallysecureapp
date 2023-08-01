12589faaaacafde8a4206ae0ad364bd66703806e
allengayCx <allen.gay@checkmarx.com>
2023-08-01 10:40:50 -0700

    removed space

--- src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/SQLInjectionController.java
+++ src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/SQLInjectionController.java
@@ -65,9 +65,7 @@ public class SQLInjectionController extends AbstractController {
                     }
 				});
 		//*/ End SQLi Section
-
 		
-
 		/* Start Remediated Section
 		String sql = "SELECT name, secret from USERS where name=? or password=?";
 		return jdbcTemplate.query(sql, new PreparedStatementCallback<Boolean>(){
